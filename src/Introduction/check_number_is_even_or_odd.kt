c00221771cb1c6ed7e8a4027d273ed98c484ff32
Hossam Rakha <hossamrakha0@gmail.com>
2024-03-12 20:30:55 +0200

    check whether a number is even or odd

---
+++ src/Introduction/check_number_is_even_or_odd.kt
@@ -0,0 +1,15 @@
+package Introduction
+
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+    val reader = Scanner(System.`in`)
+
+    print("Enter a number: ")
+    val num = reader.nextInt()
+
+    if (num % 2 == 0)
+        println("$num is even")
+    else
+        println("$num is odd")
+}
