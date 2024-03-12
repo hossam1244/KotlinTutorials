2754f326028640c586273514d44a2098556dd1a5
Hossam Rakha <hossamrakha0@gmail.com>
2024-03-12 19:47:38 +0200

    compute quotient and reminder

---
+++ src/Introduction/compute_quotient_and_remainder.kt
@@ -0,0 +1,12 @@
+package Introduction
+
+fun main(args: Array<String>) {
+    val dividend = 25
+    val divisor = 4
+
+    val quotient = dividend / divisor
+    val remainder = dividend % divisor
+
+    println("Quotient = $quotient")
+    println("Remainder = $remainder")
+}
