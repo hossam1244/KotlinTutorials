fb8b10461911335dd1dde588104bde97a27d337b
Hossam Rakha <hossamrakha0@gmail.com>
2024-03-13 21:29:34 +0200

    used if condition and when statement for alphabet vowel

--- src/decision_making_and_loop/alphabet_is_vowel_or_consonant.kt
+++ src/decision_making_and_loop/alphabet_is_vowel_or_consonant.kt
@@ -1,7 +1,15 @@
 fun main(args: Array<String>) {
     val ch = 'i'
 
-    val vowelConsonant = if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') "vowel" else "consonant"
+    // using if condition
+    var vowelConsonant: String =
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') "vowel" else "consonant"
 
+
+
+    vowelConsonant = when (ch) {
+        'a', 'e', 'i', 'o', 'u' -> "vowel"
+        else -> "consonant"
+    }
     println("$ch is $vowelConsonant")
 }
