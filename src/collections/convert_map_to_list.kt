ba38b1c81765db3c67ae1e6a153bf9f715f677e2
Hossam Rakha <hossamrakha0@gmail.com>
2024-03-23 14:12:19 +0200

    convert map to list

---
+++ src/collections/convert_map_to_list.kt
@@ -0,0 +1,19 @@
+package collections
+
+fun main(args: Array<String>) {
+    val map = HashMap<Int, String>()
+    map.put(1, "a")
+    map.put(2, "b")
+    map.put(3, "c")
+    map.put(4, "d")
+    map.put(5, "e")
+
+
+    val keyList = ArrayList(map.keys)
+
+    val valueList = ArrayList(map.keys)
+
+    println("Key List: $keyList")
+    println("Value List: $valueList")
+
+}
