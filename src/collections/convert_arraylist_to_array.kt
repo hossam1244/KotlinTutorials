ff55841b84c44a71ca20b65df97e0dee0b904439
Hossam Rakha <hossamrakha0@gmail.com>
2024-03-31 23:01:22 +0200

    removed un-used comment

--- src/collections/convert_arraylist_to_array.kt
+++ src/collections/convert_arraylist_to_array.kt
@@ -4,7 +4,5 @@ fun main(args: Array<String>) {
     val arrayList: List<Int> = mutableListOf(1,2,3,4,5)
 
     val array: Array<Int> = arrayList.toTypedArray()
-
-    // printing elements of the array
     array.forEach { print(it) }
 }
